5c898d125ac400e4c2f888d99694974f793b41b2
gerrymi <gerrymibernardo@gmail.com>
2018-07-28 23:46:19 -0700

    Add parallax effect by splitting land and sky in the PokeMap component

--- src/components/PokeMap.js
+++ src/components/PokeMap.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const PokeTrainer = (props) => {
   const { mapPosition, children } = props;
+  // Parallax
+  const landPosition = `${mapPosition}px center`
+  const skyPosition = `${mapPosition/16}px top`
   return (
-    <div className='map' style={{ backgroundPosition: mapPosition }}>
+    <div className='map' style={{ backgroundPosition: landPosition }}>
+      <div className='sky' style={{ backgroundPosition: skyPosition }} />
       {children}
     </div>
   );
